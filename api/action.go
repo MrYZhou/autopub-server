60116e6b2ee7891cb954d99c0a38f08c9ba0bc92
lg <1762861715@qq.com>
2024-01-17 10:05:08 +0800

    api调整

--- api/action.go
+++ api/action.go
@@ -1,26 +1,12 @@
-package main
+package api
 
 import (
 	. "log/slog"
-	"testing"
 
 	. "github.com/MrYZhou/outil/command"
 	. "github.com/MrYZhou/outil/ssh"
 )
 
-
-func TestTransfer(t *testing.T) {
-	Info("转移") 
-}
-
-// 初始化环境
-func Myserver() *Cli{
-	host:="192.168.0.62:22"
-	user:="root"
-	password:="YH4WfLbGPasRLVhs"
-	con  ,_ := Server(host, user, password)
-	return con
-}
 /**
 发布web应用
 */
@@ -33,3 +19,15 @@ func Pubweb(webBase string) {
 	Run(webBase, "npm run build")
 }
 
+func TestTransfer(localPath,serverPath string) {
+	Info("转移") 
+}
+
+// 初始化环境
+func Myserver() *Cli{
+	host:="192.168.0.62:22"
+	user:="root"
+	password:="YH4WfLbGPasRLVhs"
+	con  ,_ := Server(host, user, password)
+	return con
+}
