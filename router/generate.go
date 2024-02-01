29e2654e4995af273343a3233ed8e1f78cd981c6
larry <1762861715@qq.com>
2024-02-01 23:54:12 +0800

    1

---
+++ router/generate.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	. "autopub-server/server"
+)
+
+// 代码生成相关
+
+func Generate(c *fiber.Ctx) error {
+	
+	return AppResult(c).Success()
+}
