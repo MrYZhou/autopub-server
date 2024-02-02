61a080de6a39cc88056bf5404ea07bb59ccd50d8
lg <1762861715@qq.com>
2024-02-02 15:06:56 +0800

    简化

--- server/action.go
+++ server/action.go
@@ -67,3 +67,18 @@ func Myserver() *Cli {
 	con, _ := Server(host, user, password)
 	return con
 }
+
+// java应用的模型
+type JarUpload struct {
+	JavaProjectPath string `json:"javaProjectPath"` // java项目根路径
+	LocalJarPath    string `json:"localJarPath"`    // 生成的jar文件路径
+	RemotePath      string `json:"remotePath"`      // 远程路径
+	PubCommand      string `json:"pubCommand"`      // 发布命令或打包命令
+	ExecCommand			string `json:"execCommand"`     // 后置发布命令
+}
+
+// web应用的模型
+type WebrUpload struct {
+	LocalPath  string `json:"localPath"`  // 本地web项目路径
+	RemotePath string `json:"remotePath"` // 远程web项目路径
+}
