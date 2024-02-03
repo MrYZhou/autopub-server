45541cef9629c66de85f784f899c8af8cc5f2835
larry <1762861715@qq.com>
2024-02-03 14:27:40 +0800

    gorm配置

--- util/orm.go
+++ util/orm.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 var node *snowflake.Node
@@ -24,6 +25,12 @@ func DbInit(url string) {
   var err error
   gormDb, err = gorm.Open(mysql.Open(url), &gorm.Config{
     Logger: logger.Default.LogMode(logger.Info),
+    NamingStrategy: schema.NamingStrategy{
+      TablePrefix: "",   // 数据库表前缀
+      SingularTable: true, // 不用给表名加复数
+      NoLowerCase: false, // 要不要把表面全小写
+    },
+    // Logger: logger.Discard, // 不输出日志
   })
   if err != nil {
     log.Println(err)
