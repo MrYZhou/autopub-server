7ce088f11353949ceb8fa996237b854bdf5404b9
larry <1762861715@qq.com>
2024-02-25 20:25:40 +0800

    静态文件服务

--- util/system.go
+++ util/system.go
@@ -22,7 +22,9 @@ func App() *fiber.App{
 	if app == nil{
 		app = fiber.New()
 		// 注册自定义中间件以转换上下文
-		app.Use(CtxMiddleware)		
+		app.Use(CtxMiddleware)
+		// 静态文件服务
+		app.Static("/file", "./static")		
 	}
 	return app
 }
