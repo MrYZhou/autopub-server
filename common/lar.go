fc200e56670ca9bb64aac8f4c5d0d3c6e54660c0
larry <1762861715@qq.com>
2025-04-27 22:20:35 +0800

    打开浏览器导致打开显示黑窗口

--- common/lar.go
+++ common/lar.go
@@ -3,6 +3,7 @@ package common
 import (
 	"os/exec"
 	"runtime"
+	"syscall"
 )
 
 func OpenBrowser(url string) error {
@@ -14,6 +15,12 @@ func OpenBrowser(url string) error {
 		cmd = exec.Command("open", url)
 	default:
 		cmd = exec.Command("xdg-open", url)
+	}
+	if runtime.GOOS == "windows" {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			HideWindow:    true,       // 关键参数：隐藏子进程窗口
+			CreationFlags: 0x08000000, // 可选参数：独立控制台
+		}
 	}
 	return cmd.Start() // 异步执行并返回潜在错误
 }
