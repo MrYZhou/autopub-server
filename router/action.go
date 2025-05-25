c9d3d89d3b78696a6573f8d94606973c8d9d597f
larry <1762861715@qq.com>
2025-05-25 23:37:36 +0800

    优化同时支持数组和字符串参数

--- router/action.go
+++ router/action.go
@@ -18,11 +18,7 @@ func init() {
 
 	// 创建子路由
 	api := app.Group("/action")
-	api.Get("/", action)
-	app.Post("start", start)
-}
-func action(c fiber.Ctx) error {
-	return AppResult(c).Success()
+	api.Post("start", start)
 }
 func start(c fiber.Ctx) error {
 	var model ActionModel
@@ -48,38 +44,50 @@ func DoAction(cli *Cli, action Action) error {
 	} else if actionType == "uploadFile" {
 		cli.UploadFile(action.LocalPath, action.RemotePath)
 	} else {
-		for _, value := range action.ExecCommand {
-			err := Run(action.WorkPath, value)
+		switch execCommand := action.ExecCommand.(type) {
+		case string:
+			err := Run(action.WorkPath, execCommand)
 			if err != nil {
 				return err
 			}
+		case []string:
+			for _, value := range execCommand {
+				err := Run(action.WorkPath, value)
+				if err != nil {
+					return err
+				}
+			}
 		}
 	}
-	if action.PostCommand != nil {
-		for _, value := range action.PostCommand {
+
+	switch postCommand := action.PostCommand.(type) {
+	case string:
+		cli.Run(postCommand)
+	case []string:
+		for _, value := range postCommand {
 			cli.Run(value)
 		}
 	}
+
 	if action.Name != "" {
 		log.Println("\n<========== " + action.Name + "结束 ==========>")
 	}
 	return nil
 }
 
-// java应用的模型
 type ActionModel struct {
 	Actions []Action `json:"actions"` // 操作步骤
 }
 
 type Action struct {
-	Name        string   `json:"name"`        // 步骤名称
-	WorkPath    string   `json:"workPath"`    // 工作路径,此项不空则会在此目录为根执行操作
-	LocalPath   string   `json:"localPath"`   // 本地路径
-	RemotePath  string   `json:"remotePath"`  // 远程路径
-	ActionType  string   `json:"actionType"`  // 动作类型, upload,common, 默认是common
-	ExecCommand []string `json:"execCommand"` // 命令组,需要按顺序执行的命令
-	ServerId    string   `json:"serverId"`    // 服务器id,用来标识用哪一个服务器来执行动作,保留key
-	PostCommand []string `json:"postCommand"` // 服务器上传后执行什么命令
+	Name        string      `json:"name"`        // 步骤名称
+	WorkPath    string      `json:"workPath"`    // 工作路径,此项不空则会在此目录为根执行操作
+	LocalPath   string      `json:"localPath"`   // 本地路径
+	RemotePath  string      `json:"remotePath"`  // 远程路径
+	ActionType  string      `json:"actionType"`  // 动作类型, upload,common, 默认是common
+	ExecCommand interface{} `json:"execCommand"` // 命令组,需要按顺序执行的命令
+	ServerId    string      `json:"serverId"`    // 服务器id,用来标识用哪一个服务器来执行动作,保留key
+	PostCommand interface{} `json:"postCommand"` // 服务器上传后执行什么命令
 }
 
 // 初始化环境
@@ -100,5 +108,4 @@ func Getserver() *Cli {
 		con, _ := ConnectServer(cli)
 		return con
 	}
-
 }
