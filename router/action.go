30a879a6b573c27c59fa938198d45c18ff75cd11
larry <1762861715@qq.com>
2025-05-25 23:23:22 +0800

    步骤流完成

--- router/action.go
+++ router/action.go
@@ -1,13 +1,14 @@
 package router
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v3"
 
 	. "autopub-server/util"
 
+	. "github.com/MrYZhou/outil/command"
 	. "github.com/MrYZhou/outil/ssh"
 )
 
@@ -29,14 +30,40 @@ func start(c fiber.Ctx) error {
 	if err := c.Bind().Body(&model); err != nil {
 		return AppResult(c).Fail("请求体数据解析错误")
 	}
-	actions := model.Actions
-	for index:=0; index < len(actions) ; index++ {
-		action := actions[index]
-		fmt.Println(action.WorkPath)
+	con := Getserver()
+	defer con.Client.Close()
+	defer con.SftpClient.Close()
+	for _, action := range model.Actions {
+		DoAction(con, action)
 	}
-	// cli := Getserver()
-	// println(cli)
-	return AppResult(c).Success(actions)
+	return AppResult(c).Success()
+}
+func DoAction(cli *Cli, action Action) error {
+	if action.Name != "" {
+		log.Println("\n<========== " + action.Name + "开始 ==========>")
+	}
+	var actionType = action.ActionType
+	if actionType == "uploadDir" {
+		cli.UploadDir(action.LocalPath, action.RemotePath)
+	} else if actionType == "uploadFile" {
+		cli.UploadFile(action.LocalPath, action.RemotePath)
+	} else {
+		for _, value := range action.ExecCommand {
+			err := Run(action.WorkPath, value)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	if action.PostCommand != nil {
+		for _, value := range action.PostCommand {
+			cli.Run(value)
+		}
+	}
+	if action.Name != "" {
+		log.Println("\n<========== " + action.Name + "结束 ==========>")
+	}
+	return nil
 }
 
 // java应用的模型
@@ -45,12 +72,14 @@ type ActionModel struct {
 }
 
 type Action struct {
+	Name        string   `json:"name"`        // 步骤名称
 	WorkPath    string   `json:"workPath"`    // 工作路径,此项不空则会在此目录为根执行操作
 	LocalPath   string   `json:"localPath"`   // 本地路径
 	RemotePath  string   `json:"remotePath"`  // 远程路径
 	ActionType  string   `json:"actionType"`  // 动作类型, upload,common, 默认是common
 	ExecCommand []string `json:"execCommand"` // 命令组,需要按顺序执行的命令
 	ServerId    string   `json:"serverId"`    // 服务器id,用来标识用哪一个服务器来执行动作,保留key
+	PostCommand []string `json:"postCommand"` // 服务器上传后执行什么命令
 }
 
 // 初始化环境
@@ -61,7 +90,7 @@ func Getserver() *Cli {
 		return con
 	} else {
 		contentBytes, err := os.ReadFile(os.Getenv("rsaFilePath"))
-		if err == nil {
+		if err != nil {
 			panic("ssh密钥不存在")
 		}
 		var cli Cli
